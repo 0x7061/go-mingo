8200df8b612eeee97e2305c98d58cc6ab29318e5
Aleksandar Palic <palic@gmx.at>
2019-03-04 17:12:54 +0100

    Add support for map[string]interface{} when resolving

--- util.go
+++ util.go
@@ -1,6 +1,8 @@
 package mingo
 
-import "strings"
+import (
+	"strings"
+)
 
 // resolve method takes an object and selector and traverses the object by
 // the selector to return the final value.
@@ -9,11 +11,19 @@ func resolve(obj Object, selector string) interface{} {
 
 	if len(path) > 1 {
 		if obj[path[0]] != nil {
+			newObj := Object{}
+
 			switch obj[path[0]].(type) {
-			case Object:
-				newObj := obj[path[0]]
+			case map[string]interface{}:
+				for k, v := range obj[path[0]].(map[string]interface{}) {
+					newObj[k] = v
+				}
 				path = append(path[:0], path[0+1:]...)
-				return resolve(newObj.(Object), strings.Join(path, "."))
+				return resolve(newObj, strings.Join(path, "."))
+			case Object:
+				newObj = obj[path[0]].(Object)
+				path = append(path[:0], path[0+1:]...)
+				return resolve(newObj, strings.Join(path, "."))
 			}
 			return nil
 		}
